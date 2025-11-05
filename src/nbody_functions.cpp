99fd693c8e6971073e10a36f0bc75a6d75193546
SeanSaturley-UNCC <ssaturle@charlotte.edu>
2025-11-04 21:04:42 -0500

    fixed and formatted nbody_functions

--- src/nbody_functions.cpp
+++ src/nbody_functions.cpp
@@ -5,17 +5,18 @@
 #include <cmath>
 #include <stdexcept>
 
-double G = 6.674e-11; // gravitational constant
+double G = 6.674e-11; 
 
 simulation::simulation(size_t nb)
-  : nbpart(nb),
-    mass(nb),
+  : nbpart(nb), mass(nb),
+    // postition
     x(nb), y(nb), z(nb),
+    // velocity
     vx(nb), vy(nb), vz(nb),
+    // force
     fx(nb), fy(nb), fz(nb)
 {}
 
-// ---------- Initialization ----------
 void random_init(simulation& s) {
     std::mt19937 gen(std::random_device{}());
     std::uniform_real_distribution<double> dismass(0.9, 1.0);
@@ -23,9 +24,11 @@ void random_init(simulation& s) {
 
     for (size_t i = 0; i < s.nbpart; ++i) {
         s.mass[i] = dismass(gen);
+      
         s.x[i] = dispos(gen);
         s.y[i] = dispos(gen);
         s.z[i] = 0.0;
+      
         s.vx[i] = s.y[i] * 1.5;
         s.vy[i] = -s.x[i] * 1.5;
         s.vz[i] = 0.0;
@@ -35,6 +38,8 @@ void random_init(simulation& s) {
 void init_solar(simulation& s) {
     enum Planets {SUN, MERCURY, VENUS, EARTH, MARS, JUPITER, SATURN, URANUS, NEPTUNE, MOON};
     s = simulation(10);
+
+    // Masses in kg
     s.mass[SUN]     = 1.9891e30;
     s.mass[MERCURY] = 3.285e23;
     s.mass[VENUS]   = 4.867e24;
@@ -46,6 +51,7 @@ void init_solar(simulation& s) {
     s.mass[NEPTUNE] = 1.024e26;
     s.mass[MOON]    = 7.342e22;
 
+    // Positions (in meters) and velocities (in m/s)
     double AU = 1.496e11;
     s.x = {0, 0.39*AU, 0.72*AU, 1.0*AU, 1.52*AU, 5.20*AU, 9.58*AU, 19.22*AU, 30.05*AU, 1.0*AU + 3.844e8};
     s.y.assign(10, 0.0);
@@ -92,7 +98,8 @@ void update_position(simulation& s, size_t i, double dt) {
     s.z[i] += s.vz[i] * dt;
 }
 
-// ---------- Output ----------
+
+
 void dump_state(const simulation& s) {
     std::cout << s.nbpart << '\t';
     for (size_t i = 0; i < s.nbpart; ++i) {
